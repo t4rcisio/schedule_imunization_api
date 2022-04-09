82fd778b7cda498c5bf9476b5809bdeab0fc1eef
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-04-09 15:53:27 -0300

    config

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,9 +2,11 @@ class UserController {
   async Create(request, response) {
     response.send("Create user");
   }
+
   async Update(request, response) {
     response.send("Update user");
   }
+
   async Delete(request, response) {
     response.send("Delete user");
   }
