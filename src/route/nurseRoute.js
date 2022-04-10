c4f95e5855e72320293f3f5f1b54ce0d7703dbec
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-04-09 22:43:39 -0300

    route

--- src/route/nurseRoute.js
+++ src/route/nurseRoute.js
@@ -1,10 +1,18 @@
 import Route from "express";
 import UserController from "../controller/nurseController.js";
+import { NurseRules, NurseValidation } from "../utils/validator.js";
 
 const route = Route();
 const clientController = new UserController();
 
-route.post("/new", clientController.Create.bind(clientController));
+//route.use(Validator);
+
+route.post(
+  "/new",
+  NurseRules(),
+  NurseValidation,
+  clientController.Create.bind(clientController)
+);
 route.post("/profile", clientController.Update.bind(clientController));
 route.delete("/delete", clientController.Delete.bind(clientController));
 route.get("/find", clientController.FindOnebyID.bind(clientController));
