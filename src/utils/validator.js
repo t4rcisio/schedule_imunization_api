f927d431185a7070e49971d42fbaf099df471731
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-04-09 23:07:40 -0300

    validator

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,12 +1,19 @@
 import { check, validationResult } from "express-validator";
 
-const NurseRules = () => [
-  check("name").isLength({ min: 2 }),
-  check("password").isLength({ min: 8, max: 30 }),
-  check("cpf").isLength({ min: 11, max: 11 }),
-];
+const NurseRules = (method) => {
+  switch (method) {
+    case "POST": {
+      return [
+        check("name").isLength({ min: 2 }),
+        check("password").isLength({ min: 8, max: 30 }),
+        check("cpf").isLength({ min: 11, max: 11 }),
+      ];
+    }
+  }
+};
 
 const NurseValidation = (request, response, next) => {
+  NurseRules(request.method);
   const errorRules = validationResult(request);
   if (!errorRules.isEmpty())
     return response.send("<h2>Failed to process request</h2>").status(422);
@@ -14,4 +21,4 @@ const NurseValidation = (request, response, next) => {
   next();
 };
 
-export { NurseRules, NurseValidation };
+export default NurseValidation;
