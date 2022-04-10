f4f0d58438d745099eb174d963b91dcfe12fb033
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-04-10 14:35:02 -0300

    update validation params

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,19 +1,50 @@
 import { check, validationResult } from "express-validator";
 
-const NurseRules = (method) => {
-  switch (method) {
-    case "POST": {
-      return [
-        check("name").isLength({ min: 2 }),
-        check("password").isLength({ min: 8, max: 30 }),
-        check("cpf").isLength({ min: 11, max: 11 }),
-      ];
-    }
+const NurseRules = (url) => {
+  switch (url) {
+    case "nurse/new":
+      {
+        return [
+          check("name").isLength({ min: 2 }),
+          check("password").isLength({ min: 8, max: 30 }),
+          check("cpf").isLength({ min: 11, max: 11 }),
+        ];
+      }
+      break;
+    case "nurse/edit":
+      {
+        return [
+          check("name").isLength({ min: 2 }),
+          check("cpf").isLength({ min: 11, max: 11 }),
+        ];
+      }
+      break;
+    case "nurse/restore-password":
+      {
+        return [
+          check("current").isLength({ min: 8, max: 30 }),
+          check("new").isLength({ min: 8, max: 30 }),
+          check("new-repeat").isLength({ min: 8, max: 30 }),
+        ];
+      }
+      break;
+    case "nurse/login":
+      {
+        return [
+          check("cpf").isLength({ min: 11, max: 11 }),
+          check("password").isLength({ min: 8, max: 30 }),
+        ];
+      }
+      break;
+    case "nurse/delete":
+      {
+        return [check("cpf").isLength({ min: 11, max: 11 })];
+      }
+      break;
   }
 };
 
 const NurseValidation = (request, response, next) => {
-  NurseRules(request.method);
   const errorRules = validationResult(request);
   if (!errorRules.isEmpty())
     return response.send("<h2>Failed to process request</h2>").status(422);
@@ -21,4 +52,4 @@ const NurseValidation = (request, response, next) => {
   next();
 };
 
-export default NurseValidation;
+export { NurseRules, NurseValidation };
