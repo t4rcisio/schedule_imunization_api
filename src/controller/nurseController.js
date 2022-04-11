f9e384c1b3ef94360d5ef7e4ad844d160485ffe0
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-04-11 14:23:47 -0300

    update controller params

--- src/controller/nurseController.js
+++ src/controller/nurseController.js
@@ -1,9 +1,71 @@
+import bcrypt from "bcryptjs";
 import Controller from "./controller.js";
+import jsonwebtoken from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
 class UserController extends Controller {
   constructor() {
     super("Nurse_user");
   }
+
+  HashPassword(password) {
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
+  }
+
+  async Create(request, response) {
+    // Prisma does't support @unique parameter on Mongodb yet,
+    // so, I have to do it manually
+    const user = await super.Unic_cpf(request);
+    if (user) return response.send("CPF is already in use");
+
+    // Transform password in a hash
+    request.body.password = this.HashPassword(request.body.password);
+    return await super.Create(request, response);
+  }
+
+  async Login(request, response) {
+    const user = await super.Login(request);
+
+    if (user.error)
+      return response.json({ error: "Unable to connect data server" });
+    if (!user.Data)
+      return response.send({ error: "Incorrect login or password" });
+
+    const { password } = request.body;
+    const hashPassword = user.Data.password;
+    const hash = bcrypt.compareSync(password, hashPassword);
+
+    if (!hash) return response.send({ error: "Incorrect login or password" });
+
+    const { id, name } = user.Data;
+    const client = {
+      id,
+      name,
+    };
+
+    const token = jsonwebtoken.sign(client, process.env.SECRET_KEY_TOKEN, {
+      expiresIn: "8h",
+    });
+
+    response.cookie("token", token, {
+      maxAge: 60 * 60 * 8,
+      httpOnly: true,
+      secure: true,
+    });
+
+    return response.send(token);
+  }
+
+  async Update(request, response){
+
+    
+
+  }
+
+
+
 }
 
 export default UserController;
