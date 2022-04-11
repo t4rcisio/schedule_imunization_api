265d5770d9000f16ed799be44934d1790fb366bd
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-04-11 14:24:49 -0300

    update validation params

--- src/utils/nurseValidator.js
+++ src/utils/nurseValidator.js
@@ -1,44 +1,49 @@
-import { check, validationResult } from "express-validator";
+import { param, check, validationResult } from "express-validator";
 
 const NurseRules = (url) => {
   switch (url) {
     case "new":
       {
         return [
-          check("name").isLength({ min: 2 }),
-          check("password").isLength({ min: 8, max: 30 }),
-          check("cpf").isLength({ min: 11, max: 11 }),
+          check("name").trim().isLength({ min: 2 }),
+          check("password").trim().isLength({ min: 8, max: 30 }),
+          check("cpf").trim().isLength({ min: 11, max: 11 }),
         ];
       }
       break;
     case "edit":
       {
         return [
-          check("name").isLength({ min: 2 }),
-          check("cpf").isLength({ min: 11, max: 11 }),
+          param("id").trim().isString(),
+          check("name").trim().isLength({ min: 2 }),
+          check("cpf").trim().isLength({ min: 11, max: 11 }),
         ];
       }
       break;
     case "restore-password":
       {
         return [
-          check("current").isLength({ min: 8, max: 30 }),
-          check("new").isLength({ min: 8, max: 30 }),
-          check("new-repeat").isLength({ min: 8, max: 30 }),
+          param("id").trim().isString(),
+          check("current").trim().isLength({ min: 8, max: 30 }),
+          check("new").trim().isLength({ min: 8, max: 30 }),
+          check("new-repeat").trim().isLength({ min: 8, max: 30 }),
         ];
       }
       break;
     case "login":
       {
         return [
-          check("cpf").isLength({ min: 11, max: 11 }),
-          check("password").isLength({ min: 8, max: 30 }),
+          check("cpf").trim().isLength({ min: 11, max: 11 }),
+          check("password").trim().isLength({ min: 8, max: 30 }),
         ];
       }
       break;
     case "delete":
       {
-        return [check("cpf").isLength({ min: 11, max: 11 })];
+        return [
+          param("id").trim().isString(),
+          check("cpf").trim().isLength({ min: 11, max: 11 }),
+        ];
       }
       break;
   }
