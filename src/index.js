6c40c21047e0e5cfb291e135664c3ded2014d37d
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-04-12 13:44:36 -0300

    router

--- src/index.js
+++ src/index.js
@@ -10,14 +10,20 @@ const port = process.env.SERVER_PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(userAuth);
+app.use(route);
 
 app.get("/", (request, response) => {
-  response.cookie(process.env.COOKIE_KEY, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNTM5OGViNDBhMDgwMTdiOTk2MmI4NiIsIm5hbWUiOiJUYXJjaXNvIiwiaWF0IjoxNjQ5NzgwNTU3LCJleHAiOjE2NDk4MDkzNTd9.Ixu0mpUesIkqNSOFfXBv2Uq9zFpAUCkndjX1AJ9iCf4", {
-    maxAge: 60 * 60 * 8,
-    httpOnly: true,
-    secure: true,
-    path: "/",
-  });
+  response.cookie(
+    process.env.COOKIE_KEY,
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNTM5OGViNDBhMDgwMTdiOTk2MmI4NiIsIm5hbWUiOiJUYXJjaXNvIiwiaWF0IjoxNjQ5NzgwNTU3LCJleHAiOjE2NDk4MDkzNTd9.Ixu0mpUesIkqNSOFfXBv2Uq9zFpAUCkndjX1AJ9iCf4",
+    {
+      maxAge: 60 * 60 * 8,
+      httpOnly: true,
+      secure: true,
+      path: "/",
+    }
+  );
   response.redirect(
     url.format({
       pathname: "./nurse/edit/625398eb40a08017b9962b86",
@@ -29,10 +35,6 @@ app.get("/", (request, response) => {
     })
   );
 });
-});
-
-app.use(userAuth);
-app.use(route);
 
 app.listen(port, () => {
   console.log("Server running");
